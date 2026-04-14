87e4f03ec76fa81ef8f640af0f2f80ecdd71345b
Ramzy Al-Mulla <67399156+ramzymulla@users.noreply.github.com>
2026-04-13 21:49:00 -0700

    Add files via upload

--- get_target_radius.py
+++ get_target_radius.py
@@ -11,14 +11,16 @@ def process(value):
     # print(value[1])
     da,dx,dy = value.Item1,value.Item2,value.Item3
 
+    headingAngle = math.atan2(dy, dx)
+
     v = math.hypot(dx, dy)/dur # px/s
     angVel = abs(da)/dur # rad/s
     orbitalRadius = min(v/angVel if angVel > 0 else float(60000), 1000) # px
     # print(v)
     if v > 500 and 0:
         
-        targetRadius = max(angVel, minTargetRadius)
+        targetRadius = max(headingAngle*100, minTargetRadius)
         # print(angVel)
     else: 
         targetRadius = minTargetRadius
-    return (targetRadius,angVel,v,orbitalRadius)
+    return (targetRadius,angVel,v,orbitalRadius,headingAngle)
